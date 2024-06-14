c14369670c15a19be9316ac86fc5a89e433c3c55
Mårten Jern <qcc.1992@gmail.com>
2024-06-14 18:03:52 +0300

    Solution for 1.1.

--- part1/fullstack/src/App.jsx
+++ part1/fullstack/src/App.jsx
@@ -14,11 +14,18 @@ const App = () => {
       </h1>
     </div>
   )
+
+  const Part = (props) => (
+    <div>
+      <p>{props.part}: {props.exercises}</p>
+    </div>
+  )
+
   const Content = (props) => (
     <div>
-      <p>{props.part1}: {props.exercises1}</p>
-      <p>{props.part2}: {props.exercises2}</p>
-      <p>{props.part3}: {props.exercises3}</p>
+      <Part part = {part1} exercises = {exercises1}/>
+      <Part part = {part2} exercises = {exercises2}/>
+      <Part part = {part3} exercises = {exercises3}/>
     </div>
   )
   const Total = (props) => (
