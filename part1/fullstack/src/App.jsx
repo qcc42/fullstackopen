364e2432e77b4afc2eaa90324e2000f255871650
Mårten Jern <qcc.1992@gmail.com>
2024-06-17 16:02:49 +0300

    Solution for assignment 1.3.

--- part1/fullstack/src/App.jsx
+++ part1/fullstack/src/App.jsx
@@ -1,42 +1,25 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  const Header = (props) => (
-    <div>
-      <h1>
-        This is the header for course: {props.course}
-      </h1>
-    </div>
-  )
-
-  const Part = (props) => (
-    <div>
-      <p>{props.part}: {props.exercises}</p>
-    </div>
-  )
-
-  const Content = (props) => (
-    <div>
-      <Part part = {part1} exercises = {exercises1}/>
-      <Part part = {part2} exercises = {exercises2}/>
-      <Part part = {part3} exercises = {exercises3}/>
-    </div>
-  )
-  const Total = (props) => (
-    <p>Total: {props.exercises1 + props.exercises2 + props.exercises3}</p>
-  )
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content part1 = {part1} exercises1 = {exercises1} part2 = {part2} exercises2 = {exercises2} part3 = {part3} exercises3 = {exercises3} />
-      <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}/>
+    <h1>{course}</h1>
+    <p>{part1.name}: {part1.exercises}</p>
+    <p>{part2.name}: {part2.exercises}</p>
+    <p>{part3.name}: {part3.exercises}</p>
+    <p>Total: {part1.exercises+part2.exercises+part3.exercises}</p>
     </div>
   )
 }
