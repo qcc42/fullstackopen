930ceeeb435016f625466290a08c3cdfccf25961
qcc42 <qcc.1992@gmail.com>
2024-07-01 10:23:49 +0300

    Solution for assignment 1.12*.

--- part1/fullstack/src/App.jsx
+++ part1/fullstack/src/App.jsx
@@ -1,70 +1,32 @@
 import { useState } from 'react'
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-const StatisticsLine = (props) => {
-  return(
-    <tr>
-      <td>{props.text}</td><td> {props.value}</td>
-    </tr>
-  )
-}
-const Statistics = ({good,neutral,bad}) => {
-  return(
-    <div>
-      <table>
-      <StatisticsLine text="good:" value ={good} />
-      <StatisticsLine text="neutral:" value ={neutral} />
-      <StatisticsLine text="bad:" value ={bad} />
-      <StatisticsLine text="all:" value ={good+neutral+bad} />
-      <StatisticsLine text="avr:" value ={(good-bad)/(good+neutral+bad)} />
-      <StatisticsLine text="positive:" value ={good/(good+neutral+bad)*100}/>
-      </table>
-    </div>
-  )
-}
-
-
-
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const Button = (props) => (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+  const randInd = () => {
+    setSelected(Math.round(Math.random()*anecdotes.length))
+  }
 
-  const increaseGood = () =>
-  {
-    setGood(good+1)
-  }
-  const increaseNeutral = () =>
-    {
-      setNeutral(neutral+1)
-    }
-  const increaseBad = () =>
-    {
-      setBad(bad+1)
-    }  
-  if(good+bad+neutral == 0){
-    return (
-      <div>
-      <Button handleClick = {increaseGood} text = 'good'/>
-      <Button handleClick = {increaseNeutral} text = 'neutral'/>
-      <Button handleClick = {increaseBad} text = 'bad'/>
-      </div>
-    )
-  }
   return (
     <div>
-      <Button handleClick = {increaseGood} text = 'good'/>
-      <Button handleClick = {increaseNeutral} text = 'neutral'/>
-      <Button handleClick = {increaseBad} text = 'bad'/>
-      <br/>
-      <Statistics good = {good} neutral = {neutral} bad = {bad}/>
+      <Button text = "Tell me an anecdote!" handleClick = {randInd}/>
+      <p>{anecdotes[selected]}</p>
     </div>
- 
   )
 }
 
