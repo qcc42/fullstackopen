890ccf401529645a10d362bd7c190ee508694cc2
qcc42 <qcc.1992@gmail.com>
2024-07-09 12:48:17 +0300

    Solution for assignment 2.3*.

--- part2/src/components/Content.jsx
+++ part2/src/components/Content.jsx
@@ -1,16 +1,10 @@
-import Part from './Part'
-const sum = (arr) => {
-    let sumvar = 0;
-    arr.forEach((e) => sumvar += e)
-    return (sumvar)
-    }
 const Content = ({parts}) => {
     return (
         <div>
             <ul>
                 {parts.map((part) => <li> {part} </li>)}
             </ul>
-            <p>Total number of exercises: {sum(parts.map(part => part.props.exercises))}</p>  
+            <p>Total number of exercises: {parts.map(part => part.props.exercises).reduce((prev, curr, i) => prev+curr, 0)}</p>  
         </div>
         
     )
